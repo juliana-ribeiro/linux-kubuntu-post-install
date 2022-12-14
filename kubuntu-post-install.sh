fb27af0ce4d15363a60d53e12467c7d192e4bd05
Juliana Ribeiro <silva.jurib@gmail.com>
2022-12-13 22:12:23 -0300

    fix: remove typo

--- kubuntu-post-install.sh
+++ kubuntu-post-install.sh
@@ -4,7 +4,7 @@
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 URL_DISCORD="https://discord.com/api/download?platform=linux&format=deb"
 URL_VSCODE="https://az764295.vo.msecnd.net/stable/5235c6bb189b60b01b1f49062f4ffa42384f8c91/code_1.74.0-1670260027_amd64.deb"
-URL_MYSQL_WORKBENCH_UBUNTU_22.04="https://dev.mysql.com/downloads/file/?id=514053"
+URL_MYSQL_WORKBENCH_UBUNTU_2204="https://dev.mysql.com/downloads/file/?id=514053"
 URL_ZSHRC_FILE="https://raw.githubusercontent.com/juliana-ribeiro/linux-kubuntu-post-install/master/.zshrc"
 
 DIR_PACOTES_DEB="$HOME/Downloads/Deb"
@@ -12,8 +12,6 @@ DIR_DOWNLOADS="$HOME/Downloads"
 
 PACOTES_PARA_INSTALAR=(
   nvidia-driver-390
-  curl
-  gnome-keyring
   zsh
   git
   apt-transport-https
@@ -36,7 +34,7 @@ sudo rm /var/cache/apt/archives/lock
 sudo apt update -y
   
 ## Download de pacotes externos ##
-mkdir "$DIR_DOWNLOADS"
+mkdir "$DIR_PACOTES_DEB"
 wget -c "$URL_GOOGLE_CHROME"       -P "$DIR_PACOTES_DEB"
 wget -c "$URL_DISCORD"             -P "$DIR_PACOTES_DEB"
 wget -c "$URL_MYSQL_WORKBENCH_UBUNTU_2204" -P "$DIR_PACOTES_DEB"
@@ -46,6 +44,7 @@ wget -c "$URL_ZSHRC_FILE"       -P "$DIR_DOWNLOADS"
 sudo dpkg -i $DIR_PACOTES_DEB/*.deb
 
 ## Adicionando chave pública e repositório Docker ##
+sudo apt install curl gnome-keyring -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
@@ -79,7 +78,7 @@ export NVM_DIR="$HOME/.nvm"
 nvm install --lts
 
 ## Instalando Oh! My zsh e definindo como terminal padrão ##
- sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+ yes | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
  mv /$DIR_DOWNLOADS/.zshrc $HOME/.zshrc # Substitui o arquivo .zshrc com o nvm configurado
 
 # ---------------------------------------------------------------------- #
